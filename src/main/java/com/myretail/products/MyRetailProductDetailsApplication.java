674ca6471e25dea857c7032a64d68bfade2138d1
rameshnarne <ramesh.nv@hotmail.com>
2018-03-22 07:06:33 -0500

    Fixing app bootup issues. Still pending with entity manager

--- src/main/java/com/myretail/products/MyRetailProductDetailsApplication.java
+++ src/main/java/com/myretail/products/MyRetailProductDetailsApplication.java
@@ -3,12 +3,26 @@ package com.myretail.products;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
-@EnableAutoConfiguration
 @SpringBootApplication (scanBasePackages = "com.myretail.products")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableJpaRepositories(basePackages = {"com.myretail.products.repository"})
+@EnableCaching
 public class MyRetailProductDetailsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRetailProductDetailsApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 }
--- src/main/java/com/myretail/products/configuration/RedisConfiguration.java
+++ src/main/java/com/myretail/products/configuration/RedisConfiguration.java
@@ -1,31 +1,52 @@
 package com.myretail.products.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
 
-    @Value("${redis.hostName}")
+    @Value("${redis.hostName:}")
     private String hostName;
 
-    @Value("${redis.password}")
+    @Value("${redis.password:}")
     private String password;
 
-    @Value("${redis.port}")
+    @Value("${redis.port:}")
     private Integer port;
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public JedisConnectionFactory redisConnectionFactory() {
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(configuration());
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisStandaloneConfiguration configuration() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(hostName);
+        configuration.setPassword(RedisPassword.of(password.toCharArray()));
+        configuration.setPort(port);
+        return configuration;
     }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
-       return new StringRedisTemplate(jedisConnectionFactory());
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return RedisCacheManager.create(redisConnectionFactory());
     }
 }
